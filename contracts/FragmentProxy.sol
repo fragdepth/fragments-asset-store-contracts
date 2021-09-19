02038e46c3d13dd2dc7ed5a9e26e9609a440d6b2
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-09-19 19:22:30 +0900

    copyright notices

--- contracts/FragmentProxy.sol
+++ contracts/FragmentProxy.sol
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: BUSL-1.1 */
-/* Copyright © 2021 Fragcolor Pte. Ltd. */
+/* Copyright (c) 2021 Fragdepth Inc. */
+/* Based on code licensed by Fragcolor Pte. Ltd. */
 
 pragma solidity ^0.8.7;
 
